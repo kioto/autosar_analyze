b44f6e49fd3175af08c7963e7140075965b179af
Kioto Hirahara <kioto@elcamino.redbranch.co.jp>
2016-11-23 22:53:37 +0900

    Modify error message output

--- convert_html.sh
+++ convert_html.sh
@@ -3,6 +3,13 @@
 pdf_dir=pdf
 html_dir=html
 
+which pdf2txt.py > /dev/null
+if [ $? -ne 0 ]; then
+    echo "pdf2txt.py : Not found"
+    echo "> $ pip3 install --upgrade pdfminer3k"
+    exit
+fi
+
 if [ ! -d $html_dir ]; then
     mkdir $html_dir
 fi
@@ -10,7 +17,8 @@ fi
 for pdf in $pdf_dir/*; do
     html_file=$html_dir/`basename $pdf | sed -e 's/pdf/html/'`
     echo $html_file
-    pdf2txt.py -o $html_file $pdf >& /dev/null
+    #pdf2txt.py -o $html_file $pdf 2>&1 /dev/null
+    pdf2txt.py -o $html_file $pdf > /dev/null 2>&1
 done
 
 echo done
